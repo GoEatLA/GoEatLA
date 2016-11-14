84181bba07500848dbe5af2aebb900b2d85f4ca6
Josue <josueruiz4747@gmail.com>
2016-11-14 04:36:46 -0800

    Started adding codes for yelp api

---
+++ Yelp.py
@@ -0,0 +1,11 @@
+from yelp.client import Client
+
+CONSUMER_KEY = "xr2AGjpXyVFSqOFZjukQjg"
+CONSUMER_SECRET = ""
+
+
+
+
+from yelp.oauth1_authenticator import Oauth1Authenticator
+
+auth = Oauth1Authenticator(consumer_key= CONSUMER_KEY)
