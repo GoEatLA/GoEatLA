46679989e912623fd42f9b68d447147dc655f01d
Josue Ruiz <josueruiz4747@gmail.com>
2016-11-14 19:01:26 -0800

    Added Authorization for Yelp API Calls

--- Yelp.py
+++ Yelp.py
@@ -1,11 +1,15 @@
 from yelp.client import Client
-
-CONSUMER_KEY = "xr2AGjpXyVFSqOFZjukQjg"
-CONSUMER_SECRET = ""
-
-
-
-
 from yelp.oauth1_authenticator import Oauth1Authenticator
 
-auth = Oauth1Authenticator(consumer_key= CONSUMER_KEY)
+CONSUMER_KEY = "xr2AGjpXyVFSqOFZjukQjg"
+CONSUMER_SECRET = "LWKk-n_sXFJtpSxFvyNOK4yIChA"
+TOKEN = "mz6jzjOwwNiWHm9HM0SZL3lS2vAdFiv5"
+TOKEN_SECRET = "ffYjX0GfbQ7vrWnEjTPJFQiS93w"
+
+
+auth = Oauth1Authenticator(consumer_key=CONSUMER_KEY, consumer_secret=CONSUMER_SECRET,
+                           token=TOKEN, token_secret=TOKEN_SECRET)
+client = Client(auth)
+
+resp = client.search("Los Angeles")
+print(resp)
