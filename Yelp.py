a32b69948a97062defc565ced5fb4b09edf36ac6
Josue <josueruiz4747@gmail.com>
2016-11-14 21:01:43 -0800

    Created YelpAPISearch Class

--- Yelp.py
+++ Yelp.py
@@ -1,26 +1,27 @@
 from yelp.client import Client
 from yelp.oauth1_authenticator import Oauth1Authenticator
 
-CONSUMER_KEY = "xr2AGjpXyVFSqOFZjukQjg"
-CONSUMER_SECRET = "LWKk-n_sXFJtpSxFvyNOK4yIChA"
-TOKEN = "mz6jzjOwwNiWHm9HM0SZL3lS2vAdFiv5"
-TOKEN_SECRET = "ffYjX0GfbQ7vrWnEjTPJFQiS93w"
 
+class YelpSearch:
+    CONSUMER_KEY = "xr2AGjpXyVFSqOFZjukQjg"
+    CONSUMER_SECRET = "LWKk-n_sXFJtpSxFvyNOK4yIChA"
+    TOKEN = "mz6jzjOwwNiWHm9HM0SZL3lS2vAdFiv5"
+    TOKEN_SECRET = "ffYjX0GfbQ7vrWnEjTPJFQiS93w"
 
-auth = Oauth1Authenticator(consumer_key=CONSUMER_KEY, consumer_secret=CONSUMER_SECRET,
-                           token=TOKEN, token_secret=TOKEN_SECRET)
-client = Client(auth)
+    auth = Oauth1Authenticator(consumer_key=CONSUMER_KEY, consumer_secret=CONSUMER_SECRET,
+                               token=TOKEN, token_secret=TOKEN_SECRET)
+    client = Client(auth)
 
-resp = client.search("Los Angeles").businesses
+    resp = client.search("Los Angeles").businesses
 
-dicts_to_output = [
-    {
-        'name': biz.name,
-        'id': biz.id,
-        'rating': biz.rating,
-        'review_count':biz.review_count
-    }
-    for biz in resp
-]
+    dicts_to_output = [
+        {
+            'name': biz.name,
+            'id': biz.id,
+            'rating': biz.rating,
+            'review_count':biz.review_count
+        }
+        for biz in resp
+    ]
 
-print(dicts_to_output)
+    print(dicts_to_output)
